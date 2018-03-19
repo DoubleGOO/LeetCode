f56fefd043f5159b2b635f3c8dd91ff1c09e057e
DoubleGoo <981503370@qq.com>
2018-03-19 11:23:44 +0800

    my code2

--- leetcode/src/leetcode/Solution_20170905_1.java
+++ leetcode/src/leetcode/Solution_20170905_1.java
@@ -1,6 +1,6 @@
 package leetcode;
 import java.util.Hashtable;
-//Two Sum
+//Two Sum 888
 //https://leetcode.com/problems/two-sum/description/
 public class Solution_20170905_1 {
 	public static int[] twoSum(int[] nums, int target) {
