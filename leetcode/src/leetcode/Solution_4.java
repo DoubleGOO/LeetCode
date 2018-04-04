137c62f6d161d729811d665e2b8f727ba3609ccb
DoubleGoo <981503370@qq.com>
2018-04-04 11:53:08 +0800

    剑指 +2=13

--- leetcode/src/leetcode/Solution_4.java
+++ leetcode/src/leetcode/Solution_4.java
@@ -6,7 +6,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
 //½£Ö¸
-public class Solution_20180402_2 {
+public class Solution_4 {
 	//5
 //	Stack<Integer> stack1 = new Stack<Integer>();
 //    Stack<Integer> stack2 = new Stack<Integer>();
@@ -87,21 +87,66 @@ public class Solution_20180402_2 {
 //		return dp[target-1];
 //    }
 	//11
-	public int NumberOf1(int n){
-        int index = 1;
-        int number = 0;
-     
-	    while(index!=0){
-	        if((n & index)!=0)
-	            number++;
-	        index = index << 1;
-	    }
-    	return number;
-	}
+//	public int NumberOf1(int n){
+//        int index = 1;
+//        int number = 0;
+//     
+//	    while(index!=0){
+//	        if((n & index)!=0)
+//	            number++;
+//	        index = index << 1;
+//	    }
+//    	return number;
+//	}
+	//12
+//	public double power(double base, int exponent){
+//		double result = 1;
+//		for(int i = 0;i<Math.abs(exponent);i++){
+//			result*=base;
+//		}
+//		if(exponent<0){
+//			result = 1/result;
+//		}
+//		return result;
+//	}
+	//13
+//	public static void reOrderArray(int [] array) {
+//		int evenPosition = 0;
+//		int oddPosition = 0;
+//		for(int i = 0;i<array.length;i++){
+//			if(isEven(array[i])){
+//				evenPosition = i;
+//				break;
+//			}
+//		}
+//		for(int i = array.length - 1;i>=0;i--){
+//			if(!isEven(array[i])){
+//				oddPosition = i;
+//				break;
+//			}
+//		}
+//		if(evenPosition>oddPosition)
+//			return;
+//		while(evenPosition<oddPosition){
+//			for(int j = array.length - 2;j>=0;j--){
+//				if(isEven(array[j])&&!isEven(array[j+1]))
+//					exchange(array,j,j+1);
+//			}
+//			evenPosition++;
+//			oddPosition--;
+//		}
+//    }
+//	public static void exchange(int[] array,int a,int b){
+//		int temp = array[a];
+//		array[a] = array[b];
+//		array[b] = temp;
+//	}
+//	public static boolean isEven(int number){
+//		return number%2==0;
+//	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
+
 	}
 }
 //1
